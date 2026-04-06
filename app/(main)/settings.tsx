63190c2b34e5e00a1000cd23fb809baf66763db9
栗田孟 <akito14140099@gmail.com>
2026-04-07 02:14:23 +0900

    アイコン・スクショ変更

--- app/(main)/settings.tsx
+++ app/(main)/settings.tsx
@@ -381,7 +381,7 @@ export default function SettingsScreen() {
           <View style={styles.card}>
             <View style={[styles.settingRow, { borderBottomWidth: 0 }]}>
               <Text style={styles.settingLabel}>バージョン</Text>
-              <Text style={styles.settingValue}>1.1.1</Text>
+              <Text style={styles.settingValue}>1.2.0</Text>
             </View>
           </View>
         </View>
